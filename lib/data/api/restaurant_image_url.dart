22a1aea1346b861e2a3df0301dd136b00a37abf0
amalrivel <amalrivel@gmail.com>
2025-04-24 19:14:35 +0700

    Deploy Flutter Web ke GitHub Pages

--- lib/data/api/restaurant_image_url.dart
+++
@@ -1,9 +0,0 @@
-class RestaurantImageUrl {
-  static const String _baseUrl = 'https://restaurant-api.dicoding.dev/images';
-
-  static String small(String pictureId) => '$_baseUrl/small/$pictureId';
-
-  static String medium(String pictureId) => '$_baseUrl/medium/$pictureId';
-
-  static String large(String pictureId) => '$_baseUrl/large/$pictureId';
-}
